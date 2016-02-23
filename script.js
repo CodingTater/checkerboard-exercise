08db26a9af52536d6d4a52059547f6a87987d1e9
Sean Stokke <sstokke17@gmail.com>
2016-02-23 07:22:11 -0800

    gradient board

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 for (var i = 0; i<63; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  var color1 = 'rgb(' + 0 + ',' + i*4 + ',' + 128 + ')';
+  var color2 = 'rgb(' + 0 + ',' + 255 + ',' + i*4 + ')';
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
-  tile.style.backgroundColor = color;
+  i%2  === 0 ? tile.style.backgroundColor = color1 :
+  tile.style.backgroundColor = color2;
   body.appendChild(tile);
 }
