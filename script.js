d5c704edda7d882b358184542ef4d27099e527ba
Sean Stokke <sstokke17@gmail.com>
2016-02-23 07:47:34 -0800

    failed flashing board

--- script.js
+++ script.js
@@ -4,10 +4,13 @@
 for (var i = 0; i<63; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  var a = setInterval(Math.floor(Math.random() * 256), 2000);
+  var b = setInterval(Math.floor(Math.random() * 256), 2000);
+  var c = setInterval(Math.floor(Math.random() * 256), 2000);
+  var color = 'rgb(' + a + ',' + b + ',' + c + ')';
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
   tile.style.backgroundColor = color;
   body.appendChild(tile);
-}
+};
