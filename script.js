5cf074fc0b9b8c858142a74206b8bcff070842f5
Sean Stokke <sstokke17@gmail.com>
2016-02-23 07:02:25 -0800

    randomized colors for board

--- script.js
+++ script.js
@@ -4,10 +4,10 @@
 for (var i = 0; i<63; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  body.appendChild(tile);
+  var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
-  i%2  === 0 ? tile.style.backgroundColor = "black" :
-  tile.style.backgroundColor = "red";
+  tile.style.backgroundColor = color;
+  body.appendChild(tile);
 }
