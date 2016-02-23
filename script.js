6c32ec1be63e7917831ca361ca03919187364c77
Sean Stokke <sstokke17@gmail.com>
2016-02-23 06:38:19 -0800

    finished black and red

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your JS goes here
+// console.log('Its alive');
+
+
+for (var i = 0; i<63; i++) {
+  var tile = document.createElement("div");
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(tile);
+  tile.style.width = "11.1%";
+  tile.style.float = "left";
+  tile.style.paddingBottom = "11.1%";
+  i%2  === 0 ? tile.style.backgroundColor = "black" :
+  tile.style.backgroundColor = "red";
+}
