df1b8bce987a8633f43161fea58dd60d628f2c19
Sean Stokke <sstokke17@gmail.com>
2016-02-23 15:34:28 -0800

    corrected flashing added

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // console.log('Its alive');
 
+var tile = document.createElement("div");
+var divs;
 
 for (var i = 0; i<63; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  var a = setInterval(Math.floor(Math.random() * 256), 2000);
-  var b = setInterval(Math.floor(Math.random() * 256), 2000);
-  var c = setInterval(Math.floor(Math.random() * 256), 2000);
-  var color = 'rgb(' + a + ',' + b + ',' + c + ')';
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
+  var a = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  var c = Math.floor(Math.random() * 255);
+  var color = 'rgb(' + a + ',' + b + ',' + c + ')';
   tile.style.backgroundColor = color;
   body.appendChild(tile);
 };
+
+
+var colorChange = function(){
+ for (var i = 0; i<63; i++){
+   var divs = document.getElementsByTagName("div")[i]
+   var a = Math.floor(Math.random() * 255);
+   var b = Math.floor(Math.random() * 255);
+   var c = Math.floor(Math.random() * 255);
+   var color = 'rgb(' + a + ',' + b + ',' + c + ')';
+   divs.style.backgroundColor = color;
+ };
+}
+
+setInterval(colorChange, 2000);
