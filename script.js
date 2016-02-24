af54d789320d02cc4ce43a4ce784f66c08345e52
Sean Stokke <sstokke17@gmail.com>
2016-02-24 11:50:11 -0800

    jumped through the eslint hoops

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// console.log('Its alive');
+(function() {
+  'use strict';
 
+  var tile = document.createElement('div');
+  var body = document.getElementsByTagName('body')[0];
 
-for (var i = 0; i<63; i++) {
-  var tile = document.createElement("div");
-  var body = document.getElementsByTagName("body")[0];
-  body.appendChild(tile);
-  tile.style.width = "11.1%";
-  tile.style.float = "left";
-  tile.style.paddingBottom = "11.1%";
-  i%2  === 0 ? tile.style.backgroundColor = "black" :
-  tile.style.backgroundColor = "red";
+  for (var i = 0; i < 63; i++) {
+    tile.style.width = '11.1%';
+    tile.style.float = 'left';
+    tile.style.paddingBottom = '11.1%';
+    tile.style.backgroundColor = i % 2 === 0
+    ? 'black' : 'red';
+    body.appendChild(tile);
+  }
 }
+)();
