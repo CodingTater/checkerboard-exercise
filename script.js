f122ca5289a66fa39f7496c81da2390017bd86aa
Sean Stokke <sstokke17@gmail.com>
2016-02-24 12:00:50 -0800

    eslinted like a boss

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// console.log('Its alive');
+(function() {
+  'use strict';
 
+  for (var i = 0; i < 63; i++) {
+    var tile = document.createElement('div');
+    var body = document.getElementsByTagName('body')[0];
+    var rColor = Math.floor(Math.random() * 256);
+    var gColor = Math.floor(Math.random() * 256);
+    var bColor = Math.floor(Math.random() * 256);
+    var color = 'rgb(' + rColor + ',' + gColor + ',' + bColor + ')';
 
-for (var i = 0; i<63; i++) {
-  var tile = document.createElement("div");
-  var body = document.getElementsByTagName("body")[0];
-  var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-  tile.style.width = "11.1%";
-  tile.style.float = "left";
-  tile.style.paddingBottom = "11.1%";
-  tile.style.backgroundColor = color;
-  body.appendChild(tile);
+    tile.style.width = '11.1%';
+    tile.style.float = 'left';
+    tile.style.paddingBottom = '11.1%';
+    tile.style.backgroundColor = color;
+    body.appendChild(tile);
+  }
 }
+)();
